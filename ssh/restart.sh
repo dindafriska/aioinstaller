e4bc8723504abc5515fd98dfb7a50711ad8b324b
DOBO Lite <116746091+dindafriska@users.noreply.github.com>
2022-12-11 00:25:07 +0700

    restart order

--- ssh/restart.sh
+++ ssh/restart.sh
@@ -16,6 +16,7 @@ echo -e ""
 echo -e "Starting Restart All Service"
 sleep 2
 systemctl stop ws-tls
+/etc/init.d/nginx restart
 systemctl start sslh
 systemctl restart sslh
 /etc/init.d/sslh start
@@ -46,7 +47,6 @@ systemctl restart trojan-go
 /etc/init.d/openvpn restart
 /etc/init.d/fail2ban restart
 /etc/init.d/cron restart
-/etc/init.d/nginx restart
 /etc/init.d/squid restart
 screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 1000
 screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 1000
