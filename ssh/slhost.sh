a8c199c3056076655f2507a22d1d1262ce9c32ec
DOBO Lite <116746091+dindafriska@users.noreply.github.com>
2022-12-10 23:52:14 +0700

    domain

--- ssh/slhost.sh
+++ ssh/slhost.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 red='\e[1;31m'
 green='\e[0;32m'
-NC='\e[0m'
+GREEN='\033[0;32m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+NC='\033[0m'
+yl='\e[32;1m'
+bl='\e[36;1m'
+gl='\e[32;1m'
+rd='\e[31;1m'
+mg='\e[0;95m'
+blu='\e[34m'
+op='\e[35m'
+or='\033[1;33m'
+bd='\e[1m'
+color1='\e[031;1m'
+color2='\e[34;1m'
+color3='\e[0m'
+
 MYIP=$(wget -qO- icanhazip.com);
 apt install jq curl -y
 rm -f /root/domain
@@ -17,12 +33,9 @@ mkdir -p /usr/bin/v2ray
 mkdir -p /etc/xray
 mkdir -p /etc/v2ray
 clear;
-printf "       Mod By DOBOLite
-============================================
-          Domain Update
-============================================
-
-" 
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "\E[44;1;39m              ⇱ Sytem Information ⇲             \E[0m"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
 echo "";
 read -p "Masukkan Domain Utama (Cth : google.com) : " DOMAIN
 read -p "Masukkan Sub Domain (Cth : translate.google.com) : " SUB_DOMAIN
