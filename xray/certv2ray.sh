454dcaceabf2681f5f6d6621109124cba1516899
DOBO Lite <116746091+dindafriska@users.noreply.github.com>
2022-12-11 02:36:09 +0700

    certv2ray fix

--- xray/certv2ray.sh
+++ xray/certv2ray.sh
@@ -17,12 +17,14 @@ sleep 0.5
 source /var/lib/crot/ipvps.conf
 domain=$(cat /etc/xray/domain)
 sudo lsof -t -i tcp:80 -s tcp:listen | sudo xargs kill
-cd /root/
+cd ~
 rm acme.sh
+rm -rf .acme.sh
+mkdir .acme.sh
 wget -O acme.sh https://raw.githubusercontent.com/acmesh-official/acme.sh/master/acme.sh
 bash acme.sh --install --set-default-ca --server letsencrypt
-rm acme.sh
-cd .acme.sh
+#rm acme.sh
+#cd .acme.sh
 echo "starting...., Port 80 Akan di Hentikan Saat Proses install Cert"
 echo "modified by dobo lite"
 bash acme.sh --register-account -m jonidevelopmentmail@gmail.com
